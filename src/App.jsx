b4a9c8437cde4ffc4d2a2d1e509c55d38787ceed
Felype Carlos <felype.carlosilva@gmail.com>
2025-04-22 00:37:02 -0300

    salvamento 22 04 2

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           </header>
 
 
-            <div >
+            <div>
                 <img className='imagemdobanner' src="https://shoxstore.com.br/rev-img/banner_principal_2025.webp" alt="chuteira"/>
             </div>
 </div>
@@ -95,6 +95,10 @@ function App() {
 
 <footer className="footer">
       <p className='footer-text1'>© 2025 chuteirasfc.com.br</p>
+      <img className='footer-img' src='https://cdn-icons-png.flaticon.com/512/2190/2190364.png'></img>
+      <img className='footer-img' src='https://cdn-icons-png.flaticon.com/512/20/20673.png'></img>
+      <img className='footer-img' src='https://cdn-icons-png.flaticon.com/512/121/121504.png'></img>
+      <img className='footer-img' src='https://cdn-icons-png.flaticon.com/512/60/60580.png'></img>
       <p className='footer-right'>Contato: lojachuteiras@gmail.com</p>
       <img className='footer-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiIpbfJaa1oLjJquE5qUHgGQcS8SD_7Zb8qg&s'></img>
     </footer>
---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header(){
+    return (
+      <footer className="footer">
+      <p className='footer-text1'>© 2025 chuteirasfc.com.br</p>
+      <img className='footer-img' src='https://cdn-icons-png.flaticon.com/512/2190/2190364.png'></img>
+      <img className='footer-img' src='https://cdn-icons-png.flaticon.com/512/20/20673.png'></img>
+      <img className='footer-img' src='https://cdn-icons-png.flaticon.com/512/121/121504.png'></img>
+      <img className='footer-img' src='https://cdn-icons-png.flaticon.com/512/60/60580.png'></img>
+      <p className='footer-right'>Contato: lojachuteiras@gmail.com</p>
+      <img className='footer-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiIpbfJaa1oLjJquE5qUHgGQcS8SD_7Zb8qg&s'></img>
+    </footer>
+    )
+}
